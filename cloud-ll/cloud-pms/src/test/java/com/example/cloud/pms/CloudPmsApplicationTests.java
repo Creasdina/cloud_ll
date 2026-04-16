54fd98980613d1df7e7a91ab18b666a5febc75ed
Silvan_ll <135600891+Creasdina@users.noreply.github.com>
2026-04-16 16:30:30 +0800

    Add files via upload
    首次

---
+++ cloud-ll/cloud-pms/src/test/java/com/example/cloud/pms/CloudPmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloud.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudPmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
